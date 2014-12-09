81b8227ba8ad7b01fb8221dbc54897d24115ef81
Ebski <ebbe_vig@hotmail.com>
2014-12-09 14:30:34 +0100

    Room Class done

--- Dungeon/src/dungeon/Room.java
+++ Dungeon/src/dungeon/Room.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package dungeon;
 
 /**
@@ -11,15 +10,56 @@ package dungeon;
  * @author Ebbe
  */
 public class Room {
+
     private String description;
     private Room north, south, east, west;
-    
-    public Room(String description, Room north, Room south, Room, east, Room west) {
+
+    public Room(String description, Room north, Room south, Room east, Room west) {
         this.description = description;
         this.north = north;
         this.south = south;
         this.east = east;
         this.west = west;
     }
-    
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
 }
